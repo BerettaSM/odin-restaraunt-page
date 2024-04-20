144bf5ccaf223a7e894b54cd716948d2237bfb59
BerettaSM <ramonsaviato@hotmail.com>
2024-04-19 23:38:41 -0300

    feat: initial page layout

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,3 +1,17 @@
 import '../styles/global.css';
 
-console.log('Hello!');
+{
+    // Select all styled titles and apply
+    // span to words for styling.
+    const titles = document.querySelectorAll('.styled-title');
+    titles.forEach((title) => {
+        if (!title.textContent) {
+            return;
+        }
+        title.innerHTML = title.textContent
+            .trim()
+            .split(' ')
+            .map((word) => `<span>${word}</span>`)
+            .join('');
+    });
+}
