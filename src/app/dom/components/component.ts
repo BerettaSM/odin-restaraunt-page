0f571aa7485afe66448eb6d113f852f7dded6ddd
BerettaSM <ramonsaviato@hotmail.com>
2024-04-24 22:38:33 -0300

    rearrange: add base component class

---
+++ src/app/dom/components/component.ts
@@ -0,0 +1,17 @@
+import { Renderable } from '../types';
+
+export abstract class Component implements Renderable {
+    protected children: Component[] = [];
+
+    constructor(...children: Component[]) {
+        this.children.push(...children);
+    }
+
+    abstract create(): HTMLElement | DocumentFragment;
+
+    renderOn(target: HTMLElement | DocumentFragment): void {
+        const self = this.create();
+        this.children.forEach((child) => child.renderOn(self));
+        target.appendChild(self);
+    }
+}
