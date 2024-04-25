f209d9c3f1cdcd4650188bc0b72ff8a098348279
BerettaSM <ramonsaviato@hotmail.com>
2024-04-24 22:36:23 -0300

    refactor: add function to give structure to styled titles

---
+++ src/app/dom/helpers.ts
@@ -0,0 +1,17 @@
+export function toStyledTitle(title: HTMLHeadingElement) {
+    const text = title.textContent?.trim();
+    if (!text) {
+        return title;
+    }
+    const clone = title.cloneNode(true) as HTMLHeadingElement;
+    clone.innerHTML = '';
+    clone.classList.add('styled-title');
+    text.split(' ')
+        .map((word) => {
+            const span = document.createElement('span');
+            span.textContent = word;
+            return span;
+        })
+        .forEach((span) => clone.appendChild(span));
+    return clone;
+}
