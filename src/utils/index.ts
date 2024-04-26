7fbb786577b70d40c7fa174ecc42377de1d2e9a7
BerettaSM <ramonsaviato@hotmail.com>
2024-04-25 22:54:05 -0300

    rearrange: add currency format function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,10 @@
 export function getRandomNumber(min: number, max: number) {
     return min + Math.floor(Math.random() * (max - min + 1));
 }
+
+export function formatCurrency(value: number) {
+    return value.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
+}
